6140bc0ac365ec5427f6da1fa59637c031d31c32
xdubiny <xdubiny@muni.cz>
2017-10-24 15:25:30 +0200

    tests fix

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
@@ -1,15 +1,13 @@
 package cz.fi.muni.carshop.services;
 
 import java.awt.Color;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import cz.fi.muni.carshop.CarShopStorage;
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 
 public class CarShopStorageServiceImpl implements CarShopStorageService {
 
@@ -29,7 +27,20 @@ public class CarShopStorageServiceImpl implements CarShopStorageService {
 	}
 
 	@Override
+	public void sellCar(Car car) throws RequestedCarNotFoundException {
+		Map<CarTypes, List<Car>> allCars = CarShopStorage.getInstancce().getCars();
+		List<Car> carsOfSameType = allCars.getOrDefault(car.getType(), Collections.EMPTY_LIST);
+		if(!carsOfSameType.contains(car)){
+			throw new RequestedCarNotFoundException(car+" not found.");
+		}
+		carsOfSameType.remove(car);
+	}
+
+	@Override
 	public void addCarToStorage(Car car) {
+		if(car.getPrice() < 0){
+			throw new IllegalArgumentException();z
+		}
 		CarShopStorage.getInstancce().getCars().computeIfAbsent(car.getType(), x -> new ArrayList<>()).add(car);
 	}
 
